eeb12321e9a5c7f23b67a8e251c0923c74d04acd
Sukhdev Patil <sukhadev.patil@gmail.com>
2024-05-01 18:48:37 +0530

    first commit

---
+++ src/docstrings.py
@@ -0,0 +1,19 @@
+def my_func():
+    """This is multiline comments
+    getting used to document the code
+    This would use to put information about this function
+    I can access docstring
+    my_func.__doc__
+    """
+
+
+print(my_func.__doc__)
+
+text = "Single 'quotation marks' are allowed here but \"double quotes\" must be escaped here"
+print(text)
+
+text = 'Single \'quotation marks\' must escaped but "double quotes" are allowed here'
+print(text)
+
+text = r"File path c:\users\O579957"
+print(text)
---
+++ src/string_manipulation.py
@@ -0,0 +1,25 @@
+
+print('Hi'*3)
+print('*'*10)
+
+text1 = 'I am'
+text2 = 'here...'
+print(text1 + " " + text2)
+
+print(text1, text2)
+
+print(f'Hi, {text1} {text2}')
+
+text = 'Hello Jack'
+print(text[0])
+print(text[4])
+print(text[-1])
+print(text[-9])
+
+
+print(text[:5])
+print(text[:-3])
+print(text[-3:])
+print(text[4:])
+print(text[6:10])
+print(text[-9:-6])
