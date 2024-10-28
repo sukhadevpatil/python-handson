c5d93c398113125ae38b0f1eff48d9d61f510432
Sukhdev Patil <sukhadevpatil@gmail.com>
2024-10-28 10:15:11 +0530

    few changes

--- src/test/__init__.py
+++ src/test/__init__.py
@@ -2,3 +2,23 @@
 a = 100
 
 print(a == 100)
+
+participants = ['Tom', 'Dwayne', 'Mark', 'Tim']
+newcomers = ['John', 'Maria']
+
+Bigger_List = [participants, newcomers]
+
+print(Bigger_List)
+
+participants.sort()
+print(participants)
+
+participants.sort(reverse=True)
+print(participants)
+
+Numbers = [1 , 3, 5, 2, 4]
+Numbers.sort()
+print(Numbers)
+
+Numbers.sort(reverse=True)
+print(Numbers)
