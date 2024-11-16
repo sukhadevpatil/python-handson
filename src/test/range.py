7348040bfd85c61a1067f58d47502b9d783f7e1e
Sukhdev Patil <sukhadevpatil@gmail.com>
2024-11-16 22:32:08 +0530

    range examples

--- src/test/range.py
+++ src/test/range.py
@@ -7,5 +7,15 @@ print(type(range(10)))
 
 print(list(range(1, 20, 2)))
 
+print('=======================================')
 
+x = [0, 1, 2]
+
+for item in x:
+    print(item, end = " ")
+print()
+print('=======================================')
+
+for item in list(range(len(x))):
+    print(item, end = " ")
 
